b54e7551873052f31508cc1e2a21ba1cdf681adc
Tan, Hsi-nien <hsi-nien.tan@vumc.org>
2021-08-04 05:19:23 +0000

    update src

--- src/angular-jszip-utils.module.js
+++ src/angular-jszip-utils.module.js
@@ -2,4 +2,36 @@
 
 angular
   .module('angular-jszip-utils', [])
-  .value('version', '0.1');
+  .value('version', '0.1')
+  .provider('JsZipUtils', function ChartJsProvider () {
+    var jsZipUtils = ($q, $http) => {
+      return {
+        getBinaryContent: (path, options = {}) => {
+          return $q(function(resolve, reject) {
+            let httpOption = { responseType: 'arraybuffer', eventHandlers: {} };
+            if(options.progress) {
+              httpOption.eventHandlers.progress = function(e) {
+                options.progress({
+                  path: path,
+                  originalEvent: e,
+                  percent: e.loaded / e.total * 100,
+                  loaded: e.loaded,
+                  total: e.total
+                });
+              };
+            }
+
+            $http.get(path, httpOption).then(function(response) {
+              resolve(response.data);
+            }, reject);
+
+          });
+        }
+      };
+    };
+
+
+    this.$get = ['$q', '$http', function ($q, $http) {
+      return jsZipUtils($q, $http);
+    }];
+  });
